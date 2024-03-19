f98188422d6b52488821f3b8aed58217e08bcd57
Kira Pitman <kirapitman@gmail.com>
2024-03-20 09:26:19 +1300

    delete and add necessary files

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,11 +1,8 @@
-import AddTodo from './AddTodo.tsx'
-
 function App() {
   return (
     <>
       <header className="header">
-        <h1>todos</h1>
-        <AddTodo />
+        <h1>weather!</h1>
       </header>
       <section className="main"></section>
       <footer className="footer"></footer>

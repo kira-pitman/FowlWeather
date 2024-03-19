e888651e973be4596eeb0803240adae9fbebd065
Kira Pitman <kirapitman@gmail.com>
2024-03-20 10:26:07 +1300

    delete unnecessary file

--- client/apiClient.ts
+++
@@ -1,8 +0,0 @@
-import request from 'superagent'
-import { Weather } from '../models/weather.ts'
-
-export async function getWeather(city: string): Promise<Weather> {
-  console.log('made it to get weather')
-  const response = await request.get(`${serverURL}/weather/${city}`)
-  return response.body
-}

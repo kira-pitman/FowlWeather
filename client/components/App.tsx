fd4709b27bc63961decfc0d98224eb608c345853
Kira Pitman <kirapitman@gmail.com>
2024-03-20 09:36:55 +1300

    update header1

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <header className="header">
-        <h1>weather!</h1>
+        <h1>Fowl Weather or Ducks in a Row</h1>
       </header>
       <section className="main"></section>
       <footer className="footer"></footer>

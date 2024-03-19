96ff8bbf55b047f60a30d86cffb894fcf431317c
Kira Pitman <kirapitman@gmail.com>
2024-03-20 09:57:27 +1300

    write typescript object model for weather

---
+++ models/weather.ts
@@ -0,0 +1,13 @@
+export interface Weather {
+  cloud_pct?: number
+  city_name?: string
+  temp: number
+  feels_like: number
+  humidity: number
+  min_temp: number
+  max_temp: number
+  wind_speed: number
+  wind_degrees: number
+  sunrise: number
+  sunset: number
+}
